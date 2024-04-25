ae11d55823cb941b904711e26f8b62755b1f7d4d
Becky Sweger <rsweger@umass.edu>
2024-04-25 18:08:12 -0400

    Add clade assignment function

--- data-pipeline/src/covid_variant_pipeline/assign_clades.py
+++ data-pipeline/src/covid_variant_pipeline/assign_clades.py
@@ -7,7 +7,9 @@ loggy.init_logger()
 logger = loggy.logger
 
 UPDATED_AFTER = "04/01/24"
-PACKAGE_FILE = "data/ncbi_sars-cov-2.zip"
+DATA_DIR = "data"
+PACKAGE_NAME = "ncbi_sars-cov-2"
+PACKAGE_FILE = f"{DATA_DIR}/{PACKAGE_NAME}.zip"
 
 
 def get_sequences():
@@ -31,13 +33,23 @@ def get_sequences():
         ]
     )
 
+    # unzip the data package
+    subprocess.run(
+        [
+            "unzip",
+            f"{PACKAGE_FILE}",
+            "-d",
+            f"{DATA_DIR}/",
+        ]
+    )
+
 
 def get_sequence_metadata():
     """Generate tabular representation of the downloaded genbank sequences."""
 
     logger.info("Extracting sequence metadata...")
     fields = "accession,sourcedb,sra-accs,isolate-lineage,geo-region,geo-location,isolate-collection-date,release-date,update-date,virus-pangolin,length,host-name,isolate-lineage-source,biosample-acc,completeness,lab-host,submitter-names,submitter-affiliation,submitter-country"
-    with open("data/ncbi_metadata.tsv", "w") as f:
+    with open(f"{DATA_DIR}/ncbi_metadata.tsv", "w") as f:
         subprocess.run(
             [
                 "bin/dataformat",
@@ -57,11 +69,40 @@ def get_reference_tree():
     logger.info("Reference tree download not yet implemented, using static copy: data/reference.json")
 
 
+def assign_clades():
+    """Assign downloaded genbank sequences to a clade."""
+
+    logger.info("Assigning sequences to clades...")
+    sequence_file = f"{DATA_DIR}/ncbi_dataset/data/genomic.fna"
+    assignment_file = f"{DATA_DIR}/clade_assignments.csv"
+
+    with open(f"{DATA_DIR}/clade_assignments.csv", "w") as f:
+        subprocess.run(
+            [
+                "bin/nextclade",
+                "run",
+                "--input-tree",
+                f"{DATA_DIR}/reference.json",
+                "--input-ref",
+                f"{DATA_DIR}/covid_reference_sequence.fasta",
+                "--output-csv",
+                f"{assignment_file}",
+                f"{sequence_file}",
+            ],
+            stdout=f,
+        )
+
+    return assignment_file
+
+
 def main():
     logger.info("Starting pipeline")
     get_sequences()
     get_sequence_metadata()
     get_reference_tree()
+    assignment_file = assign_clades()
+
+    logger.info(f"Sequence clade assignments are ready at {assignment_file}")
 
 
 if __name__ == "__main__":
