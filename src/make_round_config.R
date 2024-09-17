f9f5ad1f4b9993e8b55e5323daa8fdb857368de6
Becky Sweger <rsweger@umass.edu>
2024-09-17 13:25:24 -0400

    Append new round to existing task config
    Hubverse tooling doesn't yet have a built-in way to append
    a round to an existing tasks.json, so there's a few functions
    here to support that operation.

--- src/make_round_config.R
+++ src/make_round_config.R
@@ -1,8 +1,10 @@
-## create config file output for a round
+## Add a new round to the hub's task config (hub-config/tasks.json)
+## This script is part of a "round open" process run via a scheduled GitHub action and
+## should be run from the hub's root directory.
 
 library(hubAdmin)
+library(hubUtils)
 library(tools)
-
 
 get_clade_file_info <- function(directory = "auxiliary-data/modeled-clades") {
   # the round_id is the date of the most recent "clades to model"
@@ -13,82 +15,79 @@ get_clade_file_info <- function(directory = "auxiliary-data/modeled-clades") {
   return(clade_file_info)
 }
 
-
 get_clade_list <- function(filename) {
   # get a list of the clades to be modeled in this round
   clade_list <- readLines(filename)
   return(clade_list)
 }
 
+create_new_round <- function() {
+  # create a new round object
+  this_round_clade_file <- get_clade_file_info()
+  this_round_clade_list <- sort(get_clade_list(this_round_clade_file$clade_file))
+  this_round_date <- this_round_clade_file$round_id
+  if (isFALSE(weekdays(as.Date(this_round_date)) == "Wednesday")) {
+    stop("The latest clade_file does not have a Wednesday date: ", this_round_clade_file)
+  }
 
-this_round_clade_file <- get_clade_file_info()
-this_round_clade_list <- sort(get_clade_list(this_round_clade_file$clade_file))
-this_round_date <- this_round_clade_file$round_id
-if (isFALSE(weekdays(as.Date(this_round_date)) == "Wednesday")) {
-  stop("The latest clade_file does not have a Wednesday date: ", this_round_clade_file)
-}
-
-
-model_tasks <- hubAdmin::create_model_tasks(
-  hubAdmin::create_model_task(
-    task_ids = create_task_ids(
-      hubAdmin::create_task_id("nowcast_date",
-        required = list(this_round_date),
-        optional = NULL
-      ),
-      hubAdmin::create_task_id("target_date",
-        required = NULL,
-        ## target date is nowcast_date and the three prior weeks
-        optional = as.character(as.Date(this_round_date) - c(0, 7, 14, 21))
-      ),
-      hubAdmin::create_task_id("location",
-        required = NULL,
-        optional = c(
-          "AL", "AK", "AZ", "AR", "CA", "CO",
-          "CT", "DE", "DC", "FL", "GA", "HI",
-          "ID", "IL", "IN", "IA", "KS", "KY",
-          "LA", "ME", "MD", "MA", "MI", "MN",
-          "MS", "MO", "MT", "NE", "NV", "NH",
-          "NJ", "NM", "NY", "NC", "ND", "OH",
-          "OK", "OR", "PA", "RI", "SC", "SD",
-          "TN", "TX", "UT", "VT", "VA", "WA",
-          "WV", "WI", "WY", "PR"
+  model_tasks <- hubAdmin::create_model_tasks(
+    hubAdmin::create_model_task(
+      task_ids = hubAdmin::create_task_ids(
+        hubAdmin::create_task_id("nowcast_date",
+          required = list(this_round_date),
+          optional = NULL
+        ),
+        hubAdmin::create_task_id("target_date",
+          required = NULL,
+          ## target date is nowcast_date and the three prior weeks
+          optional = as.character(as.Date(this_round_date) - c(0, 7, 14, 21))
+        ),
+        hubAdmin::create_task_id("location",
+          required = NULL,
+          optional = c(
+            "AL", "AK", "AZ", "AR", "CA", "CO",
+            "CT", "DE", "DC", "FL", "GA", "HI",
+            "ID", "IL", "IN", "IA", "KS", "KY",
+            "LA", "ME", "MD", "MA", "MI", "MN",
+            "MS", "MO", "MT", "NE", "NV", "NH",
+            "NJ", "NM", "NY", "NC", "ND", "OH",
+            "OK", "OR", "PA", "RI", "SC", "SD",
+            "TN", "TX", "UT", "VT", "VA", "WA",
+            "WV", "WI", "WY", "PR"
+          )
+        ),
+        hubAdmin::create_task_id("variant",
+          required = this_round_clade_list,
+          optional = NULL
         )
       ),
-      hubAdmin::create_task_id("variant",
-        required = this_round_clade_list,
-        optional = NULL
-      )
-    ),
-    output_type = hubAdmin::create_output_type(
-      hubAdmin::create_output_type_sample(
-        is_required = FALSE,
-        output_type_id_type = "character",
-        max_length = 15L,
-        min_samples_per_task = 1L,
-        max_samples_per_task = 500L,
-        value_type = "double",
-        value_minimum = 0L,
-        value_maximum = 1L,
-      )
-    ),
-    target_metadata = hubAdmin::create_target_metadata(
-      hubAdmin::create_target_metadata_item(
-        target_id = "variant prop",
-        target_name = "Weekly nowcasted variant proportions",
-        target_units = "proportion",
-        target_keys = NULL,
-        target_type = "compositional",
-        is_step_ahead = TRUE,
-        time_unit = "week"
+      output_type = hubAdmin::create_output_type(
+        hubAdmin::create_output_type_sample(
+          is_required = FALSE,
+          output_type_id_type = "character",
+          max_length = 15L,
+          min_samples_per_task = 1L,
+          max_samples_per_task = 500L,
+          value_type = "double",
+          value_minimum = 0L,
+          value_maximum = 1L,
+        )
+      ),
+      target_metadata = hubAdmin::create_target_metadata(
+        hubAdmin::create_target_metadata_item(
+          target_id = "variant prop",
+          target_name = "Weekly nowcasted variant proportions",
+          target_units = "proportion",
+          target_keys = NULL,
+          target_type = "compositional",
+          is_step_ahead = TRUE,
+          time_unit = "week"
+        )
       )
     )
   )
-)
 
-
-new_round <- hubAdmin::create_rounds(
-  create_round(
+  round <- hubAdmin::create_round(
     "nowcast_date",
     round_id_from_variable = TRUE,
     model_tasks = model_tasks,
@@ -98,23 +97,59 @@ new_round <- hubAdmin::create_rounds(
       end = 1L
     )
   )
-)
-
-
-task_config <- hubAdmin::create_config(new_round)
-hubAdmin::write_config(task_config, overwrite = TRUE, silent = TRUE)
-
-
-valid_task_config <- validate_config(
-  hub_path = ".",
-  config = c("tasks"),
-  schema_version = "from_config",
-  branch = "main"
-)
-if (isFALSE(valid_task_config)) {
-  print(valid_task_config)
-  stop("Generated task config is invalid for round: ", this_round_date)
+  return(round)
 }
+
+write_and_validate_task_config <- function(task_config, round_id) {
+  hubAdmin::write_config(task_config, overwrite = TRUE, silent = TRUE)
+  valid_task_config <- hubAdmin::validate_config(
+    hub_path = ".",
+    config = c("tasks"),
+    schema_version = "from_config",
+    branch = "main"
+  )
+  if (isFALSE(valid_task_config)) {
+    print(valid_task_config)
+    stop("Generated task config is invalid for round: ", round_id)
+  }
+}
+
+coerce_to_round <- function(existing_round, new_round) {
+  # take round info from an existing hub tasks.config (as
+  # read by read_config()) and coerce it to a round object
+  # that will be recognized by hubAdmin's create_rounds()
+  class(existing_round) <- c("round", "list")
+  attr(existing_round, "round_id") <- attr(new_round, "round_id")
+  attr(existing_round, "schema_id") <- attr(new_round, "schema_id")
+  existing_round$model_tasks[[1]]$task_ids[["nowcast_date"]][["required"]] <-
+    list(existing_round$model_tasks[[1]]$task_ids[["nowcast_date"]][["required"]])
+  return(existing_round)
+}
+
+append_round <- function(old_task_config, new_round) {
+  # add a new round to the hub's existing task config
+  existing_round_list <- lapply(old_task_config$rounds, coerce_to_round, new_round)
+  round_list <- append(existing_round_list, list(new_round), 0)
+  rounds <- do.call(hubAdmin::create_rounds, round_list)
+  new_task_config <- hubAdmin::create_config(rounds)
+  return(new_task_config)
+}
+
+new_round <- create_new_round()
+
+existing_task_config <- try(hubUtils::read_config(".", config = c("tasks")), silent = TRUE)
+if (inherits(existing_task_config, "try-error")) {
+  print("Existing config not found, creating a new tasks.json")
+  new_task_config <- hubAdmin::create_config(hubAdmin::create_rounds(new_round))
+} else {
+  print("Existing config found, adding a new round")
+  new_task_config <- append_round(existing_task_config, new_round)
+}
+
+write_and_validate_task_config(new_task_config, this_round_date)
+print("New round added to tasks.json")
+print(new_round)
+
 
 # TODO: create a sample model-output file for this round
 # https://hubverse-org.github.io/hubData/reference/create_model_out_submit_tmpl.html
