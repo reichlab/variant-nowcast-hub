2dce2169827f22c1e87cb7045252d41bbc1685a4
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-10-01 12:37:20 -0700

    fenangle report

--- src/validations/R/clade_prop_sum_one.R
+++ src/validations/R/clade_prop_sum_one.R
@@ -1,6 +1,4 @@
 clade_prop_sum_one <- function(tbl, file_path) {
-  # 
-
   tbl <- tbl[tbl[["output_type"]] == "sample", ]
   error_object <- check_sum_one(tbl)
   check <- is.null(error_object)
@@ -9,36 +7,28 @@ clade_prop_sum_one <- function(tbl, file_path) {
     details <- NULL
     error_object <- NULL
   } else {
-    # TODO: add information about object details
-    details <- cli::format_inline("See {.var error_object} attribute for details")
+    bad_msg <- c("The following tasks had values that did not sum to one:",
+      cheap_kable(error_object)
+    )
+    # Wrapping 
+    # https://cli.r-lib.org/reference/inline-markup.html#wrapping
+    details <- paste(bad_msg, collapse = "\f")
   }
 
   hubValidations::capture_check_cnd(
     check = check,
     error = FALSE,
     file_path = file_path,
-    msg_subject = "Clade proportions in {.var value} columns",
-    msg_verbs = c("do", "do not"),
+    msg_subject = "All clade proportions in {.var value} columns",
+    msg_verbs = c("", "do not"),
     msg_attribute = "sum to 1 for each unique modeling task.",
     error_object = error_object,
     details = details
   )
 }
 
-cheap_kable <- function(tbl) {
-  n <- nrow(tbl)
-  cols <- names(tbl)
-  pads <- vapply(tbl, nchar, integer(n)) |>
-    apply(MARGIN = 2, max)
-  cpad <- nchar(cols)
-  pad <- ifelse(pads > cpad, pads, cpad)
-  tx <- lapply(seq_along(pad), function(i) format(tbl[[i]], width = pad[i]))
-  tx <- do.call("paste", c(list(sep = " | "), tx))
-  sep <- paste(strrep("-", pad), collapse = " | ")
-  hd <- lapply(seq_along(pad), function(i) format(cols[i], width = pad[i]))
-  hd <- paste(hd, collapse = " | ")
-  c(hd, sep, tx)
-}
+
+ALL_ONE <- function(dat) isTRUE(all.equal(sum(dat), 1))
 
 # modified from hubValidations:::check_values_sum1
 check_sum_one <- function (tbl) {
@@ -51,7 +41,7 @@ check_sum_one <- function (tbl) {
   group_cols <- c(group_cols, "output_type_id")
   check_tbl <- tbl %>%
     dplyr::group_by(dplyr::across(dplyr::all_of(group_cols))) %>%
-    dplyr::summarise(sum1 = isTRUE(all.equal(sum(.data[["value"]]), 1)))
+    dplyr::summarise(sum1 = ALL_ONE(.data[["value"]]), .groups = "drop")
   if (all(check_tbl$sum1)) {
     return(NULL)
   }
@@ -62,3 +52,106 @@ check_sum_one <- function (tbl) {
     dplyr::mutate(output_type = "sample")
 }
 
+## PRINTING UTILITIES ----------------------------------------------------------
+
+cheap_kable <- function(tbl) {
+  n <- nrow(tbl)
+  cols <- names(tbl)
+  pads <- vapply(tbl, nchar, integer(n))
+  if (n > 1) {
+    pads <- apply(pads, MARGIN = 2, max)
+  }
+  cpad <- nchar(cols)
+  pad <- ifelse(pads > cpad, pads, cpad)
+  tx <- lapply(seq_along(pad), function(i) format(tbl[[i]], width = pad[i]))
+  tx <- sprintf("| %s |", do.call("paste", c(list(sep = " | "), tx)))
+  hd <- lapply(seq_along(pad), function(i) format(cols[i], width = pad[i]))
+  hd <- make_table_like(hd)
+  sep <- make_table_like(strrep("-", pad))
+  # This doesn't really work at the moment -_-)
+  # https://cli.r-lib.org/articles/semantic-cli.html?q=non-breaking#non-breaking-spaces
+  gsub("\\s", "\u00a0", c(hd, sep, tx))
+}
+
+make_table_like <- function(vec) {
+  sprintf("| %s |", paste(vec, collapse = " | "))
+}
+
+## TESTS -----------------------------------------------------------------------
+
+test <- function() {
+  good_tbl <- example_tbl()
+  bad_tbl <- example_tbl(make_it_bad = TRUE)
+  stopifnot(
+    "oops: good data has an error" = is.null(check_sum_one(good_tbl)),
+    "oops: bad data does not error" = is.data.frame(check_sum_one(bad_tbl)),
+    "oops: bad data has wrong number of errors" = nrow(check_sum_one(bad_tbl)) == 2,
+    "oops: main validation returns incorrect class" = inherits(clade_prop_sum_one(good_tbl, "test"), "check_success"),
+    "oops: main validation returns incorrect class" = inherits(clade_prop_sum_one(bad_tbl, test), "check_failure"),
+    TRUE
+  )
+}
+
+example_tbl <- function(make_it_bad = FALSE) {
+  # generated from the README of the model output folder
+  to_generate <- r"{
+    res <- rvest::read_html("https://github.com/reichlab/variant-nowcast-hub/blob/main/model-output/README.md") |>
+      rvest::html_table()
+    out <- dplyr::bind_rows(res[2:3])
+    datapasta::df_paste(out)
+  }"
+  # uncomment this part to generate new data and then paste it below
+  # eval(parse(text = to_generate))
+  out <- data.frame(
+    stringsAsFactors = FALSE,
+        nowcast_date = c("2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25",
+                         "2024-09-25","2024-09-25","2024-09-25","2024-09-25"),
+         target_date = c("2024-09-23","2024-09-23",
+                         "2024-09-23","2024-09-23","2024-09-23","2024-09-24",
+                         "2024-09-24","2024-09-24","2024-09-24","2024-09-24",
+                         "2024-09-23","2024-09-23","2024-09-23","2024-09-23",
+                         "2024-09-23","2024-09-24","2024-09-24","2024-09-24",
+                         "2024-09-24","2024-09-24","2024-09-23","2024-09-23",
+                         "2024-09-23","2024-09-23","2024-09-23","2024-09-24",
+                         "2024-09-24","2024-09-24","2024-09-24","2024-09-24"),
+            location = c("MA","MA","MA","MA","MA",
+                         "MA","MA","MA","MA","MA","MA","MA","MA","MA","MA",
+                         "MA","MA","MA","MA","MA","MA","MA","MA","MA",
+                         "MA","MA","MA","MA","MA","MA"),
+               clade = c("24A","24B","24C",
+                         "recombinant","other","24A","24B","24C","recombinant"
+  ,"other",
+                         "24A","24B","24C","recombinant","other","24A",
+                         "24B","24C","recombinant","other","24A","24B","24C",
+                         "recombinant","other","24A","24B","24C","recombinant"
+  ,
+                         "other"),
+         output_type = c("mean","mean","mean","mean",
+                         "mean","mean","mean","mean","mean","mean","sample",
+                         "sample","sample","sample","sample","sample",
+                         "sample","sample","sample","sample","sample","sample"
+  ,
+                         "sample","sample","sample","sample","sample","sample"
+  ,
+                         "sample","sample"),
+      output_type_id = c(NA,NA,NA,NA,NA,NA,NA,NA,
+                         NA,NA,"MA00","MA00","MA00","MA00","MA00","MA00",
+                         "MA00","MA00","MA00","MA00","MA01","MA01","MA01",
+                         "MA01","MA01","MA01","MA01","MA01","MA01","MA01"),
+               value = c(0.1,0.2,0.05,0.6,0.05,0.12,
+                         0.18,0.02,0.6,0.08,0.1,0.2,0.05,0.6,0.05,0.12,
+                         0.18,0.02,0.6,0.08,0.1,0.2,0.05,0.6,0.05,0.12,
+                         0.18,0.02,0.6,0.08)
+  )
+  if (make_it_bad) {
+    n <- nrow(out)
+    out$value[c(n, n - 5)] <- 0.5
+  }
+  return(out)
+}
