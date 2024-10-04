f30302ee70580343dee829e33d0b2300a2b73cf4
Becky Sweger <rsweger@umass.edu>
2024-10-04 12:14:45 -0400

    Add a check to the computed location/state counts

--- src/get_location_date_counts.py
+++ src/get_location_date_counts.py
@@ -30,13 +30,28 @@ from virus_clade_utils.util.sequence import filter_covid_genome_metadata  # type
 
 
 def main(round_id: str, output_path: Path):
-    # Get a CladeTime object for the Nextstrain sequence metadata as of the round's
-    # closing time (8 PM US/Eastern on the day the round closes)
+    # Round closing time is 8 PM US/Eastern on the day the round closes
     round_close_time = datetime.strptime(round_id, "%Y-%m-%d").replace(
         hour=20, minute=0, second=0
     )
     round_close_time = round_close_time.replace(tzinfo=ZoneInfo("US/Eastern"))
-    ct = CladeTime(sequence_as_of=round_close_time.astimezone(ZoneInfo("UTC")))
+
+    location_date_df = get_location_date_counts(round_close_time, output_path)
+    test_counts(round_close_time, location_date_df)
+    location_date_df.write_csv(output_path / f"{round_id}.csv")
+
+
+def get_location_date_counts(
+    round_close_time: datetime, output_path: Path
+) -> pl.DataFrame:
+    """
+    Return a Polars DataFrame with total clade counts by location and collection date.
+    The DataFrame will have a column for each date 31 days prior to round close.
+    """
+
+    # CladeTime object expects a UTC datetime
+    round_close_utc = round_close_time.astimezone(ZoneInfo("UTC"))
+    ct = CladeTime(sequence_as_of=round_close_utc)
 
     # CladeTime objects provide a Polars LazyFrame reference to
     # Nextstrain's SARS-CoV-2 Genbank sequence metadata.
@@ -48,8 +63,8 @@ def main(round_id: str, output_path: Path):
 
     # Create a LazyFrame with all combinations of states and the
     # dates we're interested in (in this case, 31 days prior to
-    # the round's nowcast date)
-    end_date = datetime.strptime(round_id, "%Y-%m-%d").date()
+    # round close)
+    end_date = round_close_time.date()
     begin_date = end_date - timedelta(days=31)
     dates_and_locations = pl.LazyFrame(
         pl.date_range(begin_date, end_date, "1d", eager=True).alias("date")
@@ -79,7 +94,29 @@ def main(round_id: str, output_path: Path):
         .sort("location")
     )
 
-    pivot.write_csv(output_path / f"{round_id}.csv")
+    return pivot
+
+
+def test_counts(round_close_time: datetime, computed_counts: pl.DataFrame):
+    """Run checks on location/date clade counts."""
+
+    ct = CladeTime(sequence_as_of=round_close_time.astimezone(ZoneInfo("UTC")))
+
+    # Get all rows in sequence metadata that have reported a sequence
+    # with a collection date in the 31 days prior to round_close
+    begin_date = round_close_time.date() - timedelta(days=31)
+    test_data = filter_covid_genome_metadata(ct.sequence_metadata)
+    test_data = test_data.filter(pl.col("date") >= begin_date).collect()
+
+    # For the Polars dataframe computed in main(), count the number of cells
+    # reporting 1 or more sequences for a specific location/date combination
+    date_location_count = 0
+    for column in computed_counts.drop("location"):
+        date_location_count += computed_counts.filter(column > 0).height
+
+    # Above count should match the number of unique location/date
+    # combinations in the test data
+    assert test_data.unique(["date", "location"]).height == date_location_count
 
 
 if __name__ == "__main__":
