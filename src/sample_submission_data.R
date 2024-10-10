7b240f8c58d513e59576996a33bf5d854e58cb67
Nicholas G Reich <nick@umass.edu>
2024-10-10 06:29:07 -0400

    Update src/sample_submission_data.R
    Co-authored-by: Zhian N. Kamvar <zkamvar@umass.edu>

--- src/sample_submission_data.R
+++ src/sample_submission_data.R
@@ -5,5 +5,5 @@ source(here::here('src/sim_model_output.R'))
 df_sim <- sim_model_output()
 
 # Note: make sure to point to the correct path for your data, defaults to folder 'model-output'
-arrow::write_parquet(df_sim, "auxiliary-data/example-files/2024-10-02-umass-validtestsubmission.parquet")
+arrow::write_parquet(df_sim, here::here("auxiliary-data/example-files/2024-10-02-umass-validtestsubmission.parquet"))
 hubValidations::validate_model_data(here::here(), "2024-10-02-umass-validtestsubmission.parquet")
