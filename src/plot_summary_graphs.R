6627a858184a8a13a697efae7905cb95514d1c08
Nicholas Reich <nick@umass.edu>
2024-11-17 21:44:13 -0500

    added new plot_one_clade() to allow for pagination by clade rather than by location.

--- src/plot_summary_graphs.R
+++ src/plot_summary_graphs.R
@@ -7,6 +7,9 @@
 #' @param hub_path character string, path to the root of the hub from the current working directory,
 #' defaults to assume that variant-nowcast-hub/src is the working directory
 #' @param save_path path to where files will be saved, relative to the working directory
+#' @param page_by_location binary, if TRUE, each page of saved plots is for one location faceted by clade,
+#' if FALSE, each page is for one clade faceted by location
+
 #'
 #' @examples
 #' plot_summary_graphs(model_output_file = "LANL-CovTransformer/2024-11-13-LANL-CovTransformer.parquet",
@@ -16,7 +19,8 @@ plot_summary_graphs <- function(
     s3_data_date = NULL,
     baseline_clade = "24A",
     hub_path = "../",
-    save_path = "~/Downloads/"){
+    save_path = "~/Downloads/",
+    page_by_location = TRUE){
   require(arrow)
   require(dplyr)
 
@@ -66,14 +70,16 @@ plot_summary_graphs <- function(
   save_path_weekly = paste(save_path, model_id, "_weekly_", s3_data_date, ".pdf", sep = "")
   save_plots(model_output_data = dat,
              target_data = targets_other_wk,
-             save_path = save_path_weekly)
+             save_path = save_path_weekly,
+             page_by_location = page_by_location)
 
 
   ## Daily plots
   save_path_daily = paste(save_path, model_id, "_daily_", s3_data_date, ".pdf", sep = "")
   save_plots(model_output_data = dat,
              target_data = targets_other_wk,
-             save_path = save_path_daily)
+             save_path = save_path_daily,
+             page_by_location = page_by_location)
 
   ### plots on logit scale
 
@@ -117,14 +123,16 @@ plot_summary_graphs <- function(
   save_path_weekly_logit = paste(save_path, model_id, "_weekly_logit_", s3_data_date, ".pdf", sep = "")
   save_plots(model_output_data = model_output_logit,
              target_data = target_logit_wk,
-             save_path = save_path_weekly_logit)
+             save_path = save_path_weekly_logit,
+             page_by_location = page_by_location)
 
 
   ## Logit plots daily
   save_path_daily_logit = paste(save_path, model_id, "_daily_logit_", s3_data_date, ".pdf", sep = "")
   save_plots(model_output_data = model_output_logit,
              target_data = target_logit,
-             save_path = save_path_daily_logit)
+             save_path = save_path_daily_logit,
+             page_by_location = page_by_location)
 }
 
 
@@ -133,14 +141,25 @@ plot_summary_graphs <- function(
 #' @param model_output_data model output data to plot
 #' @param target_data target data to plot
 #' @param save_path path to save file to
+#' @param page_by_location binary, if TRUE, each page of saved plots is for one location faceted by clade,
+#' if FALSE, each page is for one clade faceted by location
 #'
 #' @return nothing, just saves the plots
-save_plots <- function(model_output_data, target_data, save_path) {
-  plots <- lapply(unique(model_output_data$location), function(.x)
-    plot_one_location_clades(this_location = .x,
-                             model_output_data = model_output_data,
-                             target_data = target_data))
-
+save_plots <- function(model_output_data,
+                       target_data,
+                       save_path,
+                       page_by_location = TRUE) {
+  if(page_by_location){
+    plots <- lapply(unique(model_output_data$location), function(.x)
+      plot_one_location(this_location = .x,
+                        model_output_data = model_output_data,
+                        target_data = target_data))
+  } else {
+    plots <- lapply(unique(model_output_data$clade), function(.x)
+      plot_one_clade(this_clade = .x,
+                     model_output_data = model_output_data,
+                     target_data = target_data))
+  }
   ggsave(save_path,
          gridExtra::marrangeGrob(plots, nrow=1, ncol=1),
          width = 12,
@@ -153,7 +172,7 @@ save_plots <- function(model_output_data, target_data, save_path) {
 #' @param model_output_data formatted model output data
 #' @param target_data formatted target data
 #'
-plot_one_location_clades <- function(this_location, model_output_data, target_data){
+plot_one_location <- function(this_location, model_output_data, target_data){
   require(dplyr)
   require(ggplot2)
   theme_set(theme_bw())
@@ -189,5 +208,50 @@ plot_one_location_clades <- function(this_location, model_output_data, target_da
     scale_size(name = "# of sequences") +
     facet_wrap(~clade) +
     ggtitle(paste(transftitle, "Observed and predicted frequencies of SARS-CoV-2 clades in", this_location))
+  return(p)
+}
+
+#' Plot clade data and predictions for one clade, all locations
+#'
+#' @param this_clade clade name
+#' @param model_output_data formatted model output data
+#' @param target_data formatted target data
+#'
+plot_one_clade <- function(this_clade, model_output_data, target_data){
+  require(dplyr)
+  require(ggplot2)
+  theme_set(theme_bw())
+
+  mean_data_loc <- model_output_data |>
+    filter(clade == this_clade) |>
+    group_by(target_date, clade, location) |>
+    summarize(mean = mean(value),
+              q10 = quantile(value, probs = 0.1),
+              q90 = quantile(value, probs = 0.9)) |>
+    mutate(type = "prediction") |>
+    rename(date = target_date,
+           value = mean)
+
+  target_data_clade <- filter(target_data, clade == this_clade)
+
+  if (min(target_data_clade$value) < 0 | max(target_data_clade$value) > 1) {
+    ylim <- c(-4, 4)
+    transftitle <- "Logit Transformed "
+  } else {
+    ylim <- c(0, 1)
+    transftitle <- ""
+  }
+
+  p <- mean_data_loc |>
+    ggplot(aes(x=date, y=value)) +
+    geom_point(data = target_data_clade, aes(size = total))+
+    geom_smooth(data = target_data_clade, se=FALSE, aes(weight = total))+
+    geom_line(color = "red") +
+    geom_ribbon(aes(ymin = q10, ymax = q90), fill="red", alpha = .5) +
+    scale_y_continuous(limits = ylim, name = "clade frequency") +
+    scale_x_date(NULL, date_breaks = "3 months", date_minor_breaks = "1 month") +
+    scale_size(name = "# of sequences") +
+    facet_wrap(~location) +
+    ggtitle(paste(transftitle, "Observed and predicted frequencies of SARS-CoV-2 clade", this_clade))
   return(p)
 }
