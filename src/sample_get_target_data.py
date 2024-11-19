5e1230c81648c32f7510a548a771f7af6c0ecaac
Becky Sweger <rsweger@umass.edu>
2024-11-18 22:37:16 -0500

    Adding a sample script to use when trying out assign_clades
    This is sample code for running Cladetime's assign_clades function.
    To run this against a smaller subset of data, create
    an environment variable called CLADETIME_DEMO and set it to true
    (for example on MacOS: export CLADETIME_DEMO=true)

---
+++ src/sample_get_target_data.py
@@ -0,0 +1,54 @@
+"""
+Sample script for using Cladetime's assign_clades feature
+
+To run the script manually:
+1. Install uv on your machine: https://docs.astral.sh/uv/getting-started/installation/
+2. From the root of this repo: uv run src/sample_get_target_data.py
+
+IMPORTANT NOTE! If you want to try the script on a subset of Nextstrain data
+(rather than a full dataset of sequences), set an environment variable named
+CLADETIME_DEMO to true before running the scripts.
+
+For example, on MacOS:
+export CLADETIME_DEMO=true
+"""
+
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "cladetime@git+https://github.com/reichlab/cladetime",
+# ]
+# ///
+
+from datetime import datetime, timezone
+
+from cladetime import CladeTime, sequence
+
+
+def main():
+    # instantiate CladeTime object
+    #   - sequence_as_of = use SARS-CoV-2 sequences as they existing on this date
+    #   - tree_as_of = for clade assignment, use the reference tree as it existed on this date
+    ct = CladeTime(sequence_as_of=datetime.now(timezone.utc), tree_as_of="2024-09-11")
+
+    # filter metadata down to the sequences you want to assign clades to
+    filtered_metadata = sequence.filter_metadata(
+        ct.sequence_metadata,
+        collection_min_date="2024-11-01",
+        collection_max_date="2024-11-30",
+    )
+
+    # assign clades
+    assigned_clades = ct.assign_clades(filtered_metadata)
+
+    # save summarized clades to a file
+    assigned_clades.summary.collect().write_csv("summarized_clades.csv")
+    # or output to another data structure
+    assigned_clades.summary.collect().to_pandas()
+
+    # the detailed file is larger, so if you need to save it, parquet is a good option
+    assigned_clades.detail.collect().write_parquet("detailed_clades.parquet")
+
+
+if __name__ == "__main__":
+    main()
