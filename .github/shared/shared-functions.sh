92a074d5e090758b2b92a015ebbd120ad7de6db8
Becky Sweger <rsweger@umass.edu>
2025-01-21 21:02:45 -0500

    Implement a matrix strategy for generating target data
    Now that we want to generate multiple versions of the time-series and
    oracle-output target data files, this changeset adds three major changes to the
    run-post-submissions-jobs workflow that runs after round submissions close:
    
    1. A new bash function that uses the nowcast_date of the most recent round
    to create a matrix of nowcast_dates going back 13 weeks
    2. Use the above output to add a matrix strategy to the create-target-data
    job (i.e., run the job once for each week of target/interim data we're creating)
    3. Upload the generated target data files as artifacts, which are then combined
    and submitted as a PR in the target-data-pr job.

--- .github/shared/shared-functions.sh
+++ .github/shared/shared-functions.sh
@@ -1,5 +1,7 @@
 # shared bash functions to use in workflows
 
+# Get the most recent Wednesday. Used to determine the most recent nowcast_date
+# for the variant-nowcast-hub.
 get_latest_wednesday() {
     local input_date="$1"
     if [ -n "$input_date" ]; then
@@ -11,4 +13,30 @@ get_latest_wednesday() {
             date -d'last wednesday' +%Y-%m-%d
         fi
     fi
+}
+
+# Create a series of dates formatted to work with a GitHub actions matrix
+generate_weekly_dates() {
+    if [ $# -ne 2 ]; then
+        echo "Usage: generate_weekly_dates YYYY-MM-DD number_of_weeks" >&2
+        return 1
+    fi
+
+    local start_date=$1
+    local weeks=$2
+    local current_date=$start_date
+
+    # Format for GitHub Actions matrix
+    echo -n '{"include":['
+
+    for ((i=1; i<=$weeks; i++)); do
+        current_date=$(date -d "$current_date - 7 days" +%Y-%m-%d)
+        if [ $i -eq $weeks ]; then
+            echo -n "{\"nowcast-date\":\"$current_date\"}"
+        else
+            echo -n "{\"nowcast-date\":\"$current_date\"},"
+        fi
+    done
+
+    echo ']}'
 }
