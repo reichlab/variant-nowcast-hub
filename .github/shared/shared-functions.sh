5e965f6c9c11f91db3cf1559d0f7d6e420eec7c4
Becky Sweger <bsweger@gmail.com>
2025-01-28 09:22:52 -0500

    Implement a matrix strategy for generating target data (#288)
    * Implement a matrix strategy for generating target data
    
    Now that we want to generate multiple versions of the time-series and
    oracle-output target data files, this changeset adds three major changes to the
    run-post-submissions-jobs workflow that runs after round submissions close:
    
    1. A new bash function that uses the nowcast_date of the most recent round
    to create a matrix of nowcast_dates going back 13 weeks
    2. Use the above output to add a matrix strategy to the create-target-data
    job (i.e., run the job once for each week of target/interim data we're creating)
    3. Upload the generated target data files as artifacts, which are then combined
    and submitted as a PR in the target-data-pr job.
    
    * Update .github/shared/shared-functions.sh
    
    Add a comment about the trailing comma
    
    Co-authored-by: Zhian N. Kamvar <zkamvar@umass.edu>
    
    * Stop creating the matrix entries manually
    
    h/t Zhian for the much improved way to generate the required
    data using jq
    
    * Remove --expression when generating matrix json
    
    It worked on MY machine!
    
    * Use --compact option when generating JSON
    
    GitHub complained about the multiple lines when sending the
    output of generate_weekly_dates() to $GITHUB_OUTPUT
    
    ---------
    
    Co-authored-by: Zhian N. Kamvar <zkamvar@umass.edu>

--- .github/shared/shared-functions.sh
+++ .github/shared/shared-functions.sh
@@ -1,5 +1,7 @@
 # shared bash functions to use in workflows
 
+# Get the most recent Wednesday. Used to determine the most recent nowcast_date
+# for the variant-nowcast-hub.
 get_latest_wednesday() {
     local input_date="$1"
     if [ -n "$input_date" ]; then
@@ -11,4 +13,22 @@ get_latest_wednesday() {
             date -d'last wednesday' +%Y-%m-%d
         fi
     fi
+}
+
+# Create a series of dates formatted to work with a GitHub actions matrix
+generate_weekly_dates() {
+    if [ $# -ne 2 ]; then
+        echo "Usage: generate_weekly_dates YYYY-MM-DD number_of_weeks" >&2
+        return 1
+    fi
+
+    local start_date=$1
+    local weeks=$2
+    local current_date=$start_date
+
+    # Format a series of dates for GitHub Actions matrix
+    seq 0 7 $(echo "7 * $weeks" | bc) \
+    | xargs -I {} date -d "$start_date -{} days" +%Y-%m-%d \
+    | jq -R \
+    | jq -sc '. | map({"nowcast-date": .}) | {"include": .}'
 }
