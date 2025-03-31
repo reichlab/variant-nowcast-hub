182a8f92089851060cac033a0e485e499019509e
Thomas Robacker <trobacker21@gmail.com>
2025-03-31 14:34:30 -0400

    Change target data load path. Mention nowcast date variable. Minor plot change.

--- src/plot_validation_data.R
+++ src/plot_validation_data.R
@@ -8,15 +8,15 @@ library("dplyr")
 library("ggplot2")
 library("arrow")
 here::i_am("src/plot_validation_data.R")
-           
+
 # Load validation data from target data dirs
 hub_path <- here::here()
 
 # Using newest Hub target data format
-df_validation <- arrow::read_parquet("./target-data/time-series/as_of=2025-03-25/nowcast_date=2024-12-25/timeseries.parquet")
+df_validation <- arrow::read_parquet(paste0(hub_path, "/target-data/time-series/as_of=2025-03-25/nowcast_date=2024-12-25/timeseries.parquet"))
 
 # Meta data for getting data available on reference date
-reference_date <- "2024-12-25"
+reference_date <- "2024-12-25" # Date for nowcasting submission
 s3_data_date <- as.character(as.Date(reference_date) - 2)
 targets_path_s3 <- paste0("https://covid-clade-counts.s3.amazonaws.com/",
                           s3_data_date, "_covid_clade_counts.parquet")
@@ -40,7 +40,6 @@ targets_retro <- df_retro |>
 # Create a PDF file to save the plots
 # Make argument to function eventually
 save_path = paste0(hub_path, "/plot_validation_by_location_", reference_date, ".pdf")
-#save_path = paste0(hub_path, "/plot_baseline_validation_by_location_", reference_date, ".pdf")
 pdf(save_path)
 unique_locs <- sort(unique(df_model_output$location))
 
@@ -49,7 +48,6 @@ for (this_location in unique_locs){
   targets_retro_this_location <- targets_retro |> subset((location == this_location))
   
   targets <- df_validation |>
-    #filter(!is.na(date), date >= "2024-01-01") |>
     mutate(clade = ifelse(clade %in% clades, clade, "other")) |>
     tidyr::complete(location, target_date, clade, fill = list(observation=0)) |>
     group_by(location, target_date) |>
@@ -66,7 +64,7 @@ for (this_location in unique_locs){
   colnames(targets_this_location)[3] <- "clade"
   
   p <- ggplot(df_out_this_location, aes(x = target_date, y = value)) + 
-    ggtitle(paste0("Daily Observed and Predicted Proportions \nfor model output in ", this_location, " - ", reference_date, "-UMass-HMLR")) + 
+    ggtitle(paste0("Daily Observed and Predicted Proportions \n", this_location, " - ", reference_date, "-UMass-HMLR")) + 
     theme(legend.position = "bottom", legend.justification = "center", legend.title = element_blank()) + 
     geom_point(data = targets_this_location, 
                inherit.aes = FALSE,  
@@ -75,7 +73,7 @@ for (this_location in unique_locs){
     geom_point(data = targets_retro_this_location, mapping = aes(x = date, y = value, color = "dodgerblue", size = total),
                inherit.aes = FALSE,
                alpha = 0.6) +
-    scale_color_manual(labels = c("Validated Data 2025-03-26", paste0("Training Data ", reference_date)), values = c("darkorange", "dodgerblue")) + 
+    scale_color_manual(labels = c("Validated Data 2025-03-25", paste0("Training Data ", reference_date)), values = c("darkorange", "dodgerblue")) + 
     scale_size(name = "# of sequences", range = c(1, 4)) +
     geom_line(color = "red") + 
     facet_wrap(~clade)
